3f19ab232daa4940688e51bc9d30fcf14912949f
Alpha <ngcoder@live.com>
2016-11-16 18:49:23 -0500

    * Server: Add base code

---
+++ src/edu/calvin/cs262/Tile.java
@@ -0,0 +1,32 @@
+package edu.calvin.cs262;
+
+/**
+ * A Tile class (POJO) for the tile relation
+ */
+public class Tile {
+
+    private int version;
+    private String data;
+
+    Tile() { /* a default constructor, required by Gson */  }
+
+    Tile(String data, int version) {
+        this.data = data;
+        this.version = version;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+    public String getData() {
+        return data;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+    public void setData(String data) {
+        this.data = data;
+    }
+
+}
